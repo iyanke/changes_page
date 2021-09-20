8ef95d6038d194b23ee15ebd741f2cc5b64fba24
Inna Yankelevich <inna_yan20@inbox.ru>
2021-09-20 13:18:51 +0300

    text

--- tests.bat
+++ tests.bat
@@ -4,3 +4,8 @@ echo "##teamcity[testFinished name='MyTest1.test1']"
 echo "##teamcity[testStarted name='MyTest1.test2']"
 echo "##teamcity[testFailed name='MyTest1.test2' message='failure message' details='message and stack trace']"
 echo "##teamcity[testFinished name='MyTest1.test2']"
+
+echo "##teamcity[testStarted name='MyTest1.test3']"
+echo "##teamcity[testFailed name='MyTest1.test3' message='failure message' details='message and stack trace']"
+echo "##teamcity[testFinished name='MyTest1.test3']"
+
